bdb417b901e701af9cb86dbfb52e895c29f6fe65
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-25 16:18:57 +0000

    created kitten model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_151152) do
+ActiveRecord::Schema.define(version: 2019_02_25_161637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "kittens", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "location"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_kittens_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_02_25_151152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "kittens", "users"
 end
