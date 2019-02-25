73496cfeec8cfb1dc1364b654a9d2523b5ce0ceb
Anna Pettersen <aopettersen@gmail.com>
2019-02-25 16:45:04 +0000

    Added associations and validations to models

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,3 +1,9 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+
   belongs_to :user
+  belongs_to :user, through: :bookings
+  has_many :bookings
 end
