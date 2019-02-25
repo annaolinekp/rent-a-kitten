899bf0771d6d528f3fbcdd0915cbe1351049e240
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-25 17:38:42 +0000

    created routes for kitten and bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, uniqueness: true, presence: true
-
-  has_many :bookings
-  has_many :kittens
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
