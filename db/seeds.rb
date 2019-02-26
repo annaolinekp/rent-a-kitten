8a4a12404db8c92c61f67b0e38ad41cb736606fe
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-26 12:18:55 +0000

    created seed for kittens and users added first name and last name columns to Users table in schema, added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+puts 'Creating 10 fake users...'
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "123456"
+
+  )
+  user.save!
+
+    kitten = Kitten.new(
+      name: Faker::Creature::Cat.name,
+      description: Faker::Creature::Cat.breed,
+      location: Faker::Address.city,
+      user: user
+    )
+    kitten.save!
+
+end
+puts 'Finished!'
