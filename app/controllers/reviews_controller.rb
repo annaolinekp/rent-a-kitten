707be6e0cd143a992f8aa975da363d19d326078d
Sara Scaini <sa.scaini@gmail.com>
2019-02-27 12:39:15 +0000

    created view review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,8 @@ class ReviewsController < ApplicationController
   def create
     @review.new(review_params)
     @review.kitten = Kitten.find(params[:kitten_id])
-    @review.save ? (render_to kitten_path) : (render :new)
+    @review.user = current_user
+    @review.save ? (render_to kitten_path(@kitten)) : (render :new)
   end
 
   private
