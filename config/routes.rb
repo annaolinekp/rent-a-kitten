e6058b59894fac3cfe7c39557b7ec89b42d0ce12
Anna Pettersen <aopettersen@gmail.com>
2019-02-28 13:59:21 +0000

    Created bookings index - code to be used on user

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  # resources :users do
+  #   resources :bookings, only: [:index, :show, :destroy]
+  # end
   resources :kittens do
     resources :bookings, only: [:new, :create]
     resources :reviews, only: [:new, :create]
