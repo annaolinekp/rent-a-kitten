6bd0f46ee7953830e697eeaf6dd851e493598e39
Sara Scaini <sa.scaini@gmail.com>
2019-02-28 11:30:54 +0000

    created env

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -6,4 +6,7 @@ class Kitten < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
