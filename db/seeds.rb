262a3b0fe99652734f2221ff822f12264665a2d6
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-28 17:04:25 +0000

    deleted seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,41 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
 
-Booking.destroy_all
-Kitten.destroy_all
-User.destroy_all
-
-puts 'Creating 12 fake users...'
-11.times do
-  user = User.new(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: "123456"
-
-  )
-  user.save!
-
-    kitten = Kitten.new(
-      name: Faker::Creature::Cat.name,
-      description: Faker::Creature::Cat.breed,
-      location: Faker::Address.city,
-      user: user
-    )
-    kitten.save!
-
-end
-
-kitten = Kitten.new(
-  name: 'Galileo',
-  description: 'Maine Coon kitty',
-  location: 'London',
-  user: User.first,
-  latitude: 103,
-  longitude: 100
-)
-kitten.save!
-
-puts 'Finished!'
