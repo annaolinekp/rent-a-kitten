4374f17f1fca2dbe9c0c361c651b660eb198519b
Sara Scaini <sa.scaini@gmail.com>
2019-02-28 11:09:23 +0000

    added coordinatemigration to kitten

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_115221) do
+ActiveRecord::Schema.define(version: 2019_02_28_090108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2019_02_27_115221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_kittens_on_user_id"
   end
 
